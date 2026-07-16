95324e7bcdbd80498b9c926648dbe86d0881efee
Ignacio Ojea <iojea@dm.uba.ar>
2026-07-16 10:26:12 -0300

    agrego botones selección de archivos

--- src/AsignacionesPopApp.jl
+++ src/AsignacionesPopApp.jl
@@ -229,7 +229,7 @@ module AsignacionesPopApp
                         )
                     ),900,400)
                 )
-            )
+            ))
         end
         Fugl.run(
             entorno, title="Asignación",window_width_px=400,window_height_px=700
